7409fa88cb26c573fe4f20d1d9bc6e47153babd7
Muaz Shash <59676679+MuazShash@users.noreply.github.com>
2020-11-08 09:58:30 -0500

    Final final fix
    works a bit more

--- src/content.js
+++ src/content.js
@@ -1,9 +1,3 @@
-isOn = require('index')
-
-console.log(isOn);
-
-if (isWorkC == "true") {
-    document.documentElement.innerHTML = '';
-    document.documentElement.innerHTML = 'This site is blocked';
-    document.documentElement.scrollTop = 0;
-}
+document.documentElement.innerHTML = '';
+document.documentElement.innerHTML = 'This site is blocked';
+document.documentElement.scrollTop = 0;
